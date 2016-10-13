5c6519d674e4f566540df81441d47daae60039a9
David Kebler <d@kebler.net>
2016-10-13 14:19:14 -0700

    initial commit - just a testing module

---
+++ devicetest.js
@@ -0,0 +1,50 @@
+var SerialPort = require("serialport");
+
+// enter in the command you want to test  (api buffer will be built)
+let cmdArr = [254, 175, 0];
+console.log('The command to be sent: ', cmdArr)
+
+// settings
+let apiCode = 170;
+let device = "/dev/ttyUSB0"
+let baudrate = 115200  // default is 8,N,1
+
+// variables needed
+let checksum = 0;
+let response = [];
+
+var port = new SerialPort(device, {
+    baudRate: baudrate
+});
+
+port.on('data', function(received) {
+    for (var byte of received) {
+        response.push(byte);
+    }
+    console.log('complete response: ', response)
+
+    port.close();
+});
+
+port.on('error', function(err) {
+    console.log('Error: ' + err.message);
+});
+
+port.on('open', function() {
+
+    cmdArr.unshift(cmdArr.length);
+    cmdArr.unshift(apiCode);
+    checksum = cmdArr.reduce((a, b) => a + b, 0) & 255;
+    cmdArr.push(checksum);
+    console.log('api command array: ', cmdArr)
+    let cmdBuf = new Buffer(cmdArr); // get status of bank 0
+    console.log('command as buffer: ', cmdBuf)
+
+    port.write(cmdBuf, function(err) {
+        if (err) {
+            return console.log('Error on write: ', err.message);
+        }
+        console.log('message is sent...');
+
+    });
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+**National Control Devices**  
+A nodejs/javascript Based Library for testing and programming of [NCD Devices](controleverything.com)
+
+This just a simple start of more robust repo with a CLI and several registerable modules that abstract most of the inner workings of NCD boards.  10/13/16
+
+first off from a terminal in the repo root run ```npm install```
+
+Currently there is just a simple test module  <em>testdevice.js<em>
+
+Edit the files settings of that file (at the top).  You'll need to know your device string which is OS dependent and may depend on which usb port you plug the device into. For linux users I include a bash script <em> usbcomports.sh<em>.  The baudrate seems to be the factory default so it should work. If you have issues then you'll have to fire up the base station software and check it.
+
+then....enter the NCD command in the variable ```cmdArr```, no need for the api and checksum stuff those are added... just the command ma'am.
+
+Then run ```npm start```  ...that's it.
